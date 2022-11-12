61d830e96a150ec37a77ae80cb35a70fcbac2bd9
zous910814 <zous910814@gmail.com>
2022-11-12 22:53:12 +0800

    feat(helper): add data_loader function

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,8 +1,18 @@
+import yfinance as yf
+
+
 def parse_command(user_msg_list: str):
     user_msg_list = user_msg_list.split(" ")
-    user_command_key = ["stock_code", "command", "start_date", "end_date"]
-    # list comprehension
+    user_command_key = ["symbol", "command", "start_date", "end_date"]
+    # dict comprehension
     user_command = {user_command_key[i]: user_msg_list[i]
                     for i in range(len(user_msg_list))}
 
     return user_command
+
+
+def data_loader(symbol, start_date):
+    df = yf.Ticker(symbol).history(start=start_date)
+    df['Date'] = df.index
+
+    return df
