7fa94585b782aaa1ff0f70cbbc1c2a3693e71fdd
zous910814 <zous910814@gmail.com>
2023-04-09 22:19:07 +0800

    fix(repo): conflict error

--- src/utils/indicator.py
+++ src/utils/indicator.py
@@ -117,11 +117,12 @@ class Indicators:
             self.__data['D'] = dv
 
     def macd(self):
-        
+        '''
         EMA(n) = (EMA the day before(n) * (n-1) + today's Close * 2) ÷ (n+1)
         EMA(m) = (EMA the day before(m) * (m-1) + today's Close * 2) ÷ (m+1)
         DIF = EMA(n) - EMA(m)
         MACD(x) = (MACD the day before * (x-1) + DIF * 2) ÷ (x+1)
+        '''        
         valid_status = self.valid_data()
         if (valid_status == None):
             return None
@@ -234,16 +235,16 @@ class Indicators:
         plt.savefig(picture)
         plt.clf()
         uploaded_image = im.upload_image(
+        picture, title="Uploaded with PyImgur")
+        im = pyimgur.Imgur(CLIENT_ID)
+
+        picture = "src/cache/MACD.png"
+        plt.savefig(picture)
+        plt.clf()
+        uploaded_image = im.upload_image(
             picture, title="Uploaded with PyImgur")
-            im = pyimgur.Imgur(CLIENT_ID)
 
-            picture = "src/cache/MACD.png"
-            plt.savefig(picture)
-            plt.clf()
-            uploaded_image = im.upload_image(
-                picture, title="Uploaded with PyImgur")
-
-            return uploaded_image.link
+        return uploaded_image.link
 
     def bias_line(self, start_date: str):
         valid_status = self.valid_data()
@@ -267,16 +268,16 @@ class Indicators:
         plt.savefig(picture)
         plt.clf()
         uploaded_image = im.upload_image(
+        picture, title="Uploaded with PyImgur")
+        im = pyimgur.Imgur(CLIENT_ID)
+
+        picture = "src/cache/BIAS.png"
+        plt.savefig(picture)
+        plt.clf()
+        uploaded_image = im.upload_image(
             picture, title="Uploaded with PyImgur")
-            im = pyimgur.Imgur(CLIENT_ID)
 
-            picture = "src/cache/BIAS.png"
-            plt.savefig(picture)
-            plt.clf()
-            uploaded_image = im.upload_image(
-                picture, title="Uploaded with PyImgur")
-
-            return uploaded_image.link
+        return uploaded_image.link
 
     def bollinger_band_line(self, start_date: str):
         valid_status = self.valid_data()
@@ -304,16 +305,16 @@ class Indicators:
         plt.savefig(picture)
         plt.clf()
         uploaded_image = im.upload_image(
+        picture, title="Uploaded with PyImgur")
+        im = pyimgur.Imgur(CLIENT_ID)
+
+        picture = "src/cache/Bollinger_Band.png"
+        plt.savefig(picture)
+        plt.clf()
+        uploaded_image = im.upload_image(
             picture, title="Uploaded with PyImgur")
-            im = pyimgur.Imgur(CLIENT_ID)
 
-            picture = "src/cache/Bollinger_Band.png"
-            plt.savefig(picture)
-            plt.clf()
-            uploaded_image = im.upload_image(
-                picture, title="Uploaded with PyImgur")
-
-            return uploaded_image.link
+        return uploaded_image.link
 
     def candlestick_chart(self, start_date: str):
         valid_status = self.valid_data()
